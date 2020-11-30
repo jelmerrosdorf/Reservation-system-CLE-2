d7bd486022fa7086e41b23151e6c3cb2820e95ae
ThreeMAC <bram.vd.hidde@gmail.com>
2020-11-30 16:20:02 +0100

    Added two templates to the repository

---
+++ Template.php
@@ -0,0 +1,64 @@
+<?php
+
+
+echo "<h1 id='headerOne'>Welkom op de homepagina!</h1><br>";
+?>
+<!DOCTYPE HTML>
+<header>
+    <nav>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <div class=divheader>
+            CoinPiano®
+        </div>
+    </nav>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+</header>
+
+
+<main>
+
+    <section>
+
+
+        <div>
+            <h1 id=firstheader>Coinpiano® 2.0</h1>
+
+            <p class="swag">De Coinpiano 2.0 is de laatste uitvinding van 3MAC Enterprises. Met zijn kindvriendelijke
+                design is deze goed toe te passen in zorginstanties voor kinderen. Denk hierbij aan een kinderafdeling
+                bij een ziekenhuis of een logopedist. Deze piano is natuurlijk niet je alledaagse snaarinstrument en
+                heeft muntjes in plaats van toetsen. Iets wat je niet vaak tegenkomt, dus dit zal je kind uren
+                zoethouden! :-)
+                <br>
+                <br>Ook komt dit product op de markt voor particulieren. Dit is niet alleen een ideale afleiding voor
+                kinderen voor of tijdens een procedure of een goed begin aan de muzikale reis van een leven, maar ook
+                natuurlijk een goed stuk speelgoed!
+            </p>
+            <br>
+            <p class="swag">
+                <br>
+            <p class="swag">
+                <a href="https://stud.hosted.hr.nl/0972816/wp-admin/post.php?post=49&action=edit" target="_blank"
+                   class="swag">Hier</a> kun je meer lezen over het gedachtenproces achter mijn design!
+            </p>
+        </div>
+    </section>
+
+
+    <div>
+        <section>
+            <p class="swag">Met acht individuele toetsen, of munten in dit geval, heb je ruim een hele bluestoonladder
+                tot je beschikking! Daarom is dit product goed geschikt voor de muzikale ontwikkeling van het kind.
+                <br>
+            </p>
+    </div>
+    <div>
+        <img src="https://i.imgur.com/TpqIkCa.jpg">
+        <img src="https://i.imgur.com/vfI3XOG.jpg">
+</main>
+</div>
+</section>
+<footer>
+    © 3MAC Enterprises
+</footer>";
+
+
