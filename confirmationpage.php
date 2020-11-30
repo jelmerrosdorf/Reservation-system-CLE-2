39dc49dafbca2b05a5d863932ceab9598f686773
jelmerrosdorf <jelmerr99@gmail.com>
2020-11-30 16:45:22 +0100

    Added sample text

--- confirmationpage.php
+++ confirmationpage.php
@@ -1,1 +1,13 @@
-<?php
+<html lang="Nederlands">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>confirmationpage</title>
+        <link rel="stylesheet" href="confirmationpage.css">
+    </head>
+
+    <body>
+        <div>Bedankt voor je afspraak!</div>
+    </body>
+
+</html>
