de7ec05f49856daf3bc01575d6e698febeae1824
ThreeMAC <bram.vd.hidde@gmail.com>
2020-12-07 14:19:33 +0100

    Edited confirmationpage.php after speed course Front-end.

--- confirmationpage.php
+++ confirmationpage.php
@@ -7,12 +7,24 @@
     <head>
         <meta charset="UTF-8">
         <title>confirmationpage</title>
-        <link rel="stylesheet" href="confirmationpage.css">
+        <link rel="stylesheet" href="style.css">
     </head>
+<body>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Reservering geslaagd</title>
+    <?php
+    $datumTekst = date('d-m-Y', strtotime($_POST['date']));
+    echo "Naam: " . $_POST['nameCustomer'] . "</br>";
+    echo "E-mailadres: " . $_POST['email'] . "</br>";
+    echo "Datum: " . $datumTekst . "</br>";
 
-    <body>
-        <div>Bedankt voor je afspraak!</div>
-    </body>
+    if (isset($_POST['verven'])){
+        echo '☑ Verven';
+    }
+    //opslaan in database
+    ?>
+</body>
 
 <footer>
 
