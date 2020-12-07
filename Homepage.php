db4bb8a2961f848769882126f246ebcec455f8c3
ThreeMAC <bram.vd.hidde@gmail.com>
2020-12-07 14:26:56 +0100

    Deleted the content of Homepage.php and started off with a template and some links.

--- Homepage.php
+++ Homepage.php
@@ -1,5 +1,25 @@
 <?php
 
-    echo "<h1>Welkom op de homepagina!</h1><br>";
+?>
+<!DOCTYPE HTML>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>CutKapsel</title>
+    <link rel="stylesheet" href="./style.css"/>
+</head>
+<body>
+<header>Welkom bij CutKapsel!</header>
+
+<nav>
+    <div>Portfolio</div>
+    <div>Over ons</div>
+    <div>Contact</div>
+    <div><a href="./appointmentpage.php">Reserveren</div>
+</nav>
+</body>
+</html>
+
+
 
 
