9e449bc1e1a9884a91bab512d5e42c8fbce6aea3
ThreeMAC <bram.vd.hidde@gmail.com>
2021-01-06 18:57:44 +0100

    Updated portfoliopage.php and css for image grid layout

--- portfoliopage.php
+++ portfoliopage.php
@@ -22,6 +22,56 @@
     <a class="inactive" href="./appointmentpage.php">Reserveren</a>
 </div>
 
+<!-- image sources -->
+<div class = "container">
+    <section class = "grid">
+        <div class="grid-sizer"></div>
+        <div class = "grid-item">
+            <img src = "http://rack.3.mshcdn.com/media/ZgkyMDEzLzA5LzI0L2RkL2JsYWNrX3doaXRlLmMzY2VjLnBuZwpwCXRodW1iCTEyMDB4NjI3IwplCWpwZw/6088aa87/26c/black_white_thumb.jpg">
+
+
+        </div>
+        <div class = "grid-item">
+            <img src = "http://media3.popsugar-assets.com/files/2015/08/27/708/n/2589278/76c8507697407135_GettyImages-479456327_masterEMP37T.xxxlarge/i/Theo-James.jpg">
+
+        </div>
+        <div class = "grid-item">
+            <img src = "https://upload.wikimedia.org/wikipedia/en/2/22/The-Maine-Black-And-White-Artwork.jpg">
+
+        </div>
+        <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg" />
+
+        </div>
+        <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg" />
+
+        </div>
+        <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/one-world-trade.jpg" />
+
+        </div>
+        <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/drizzle.jpg" />
+
+        </div>
+        <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+
+        </div>
+        <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/contrail.jpg" />
+
+        </div>
+        <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/golden-hour.jpg" />
+        </div>
+        <div class="grid-item">
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/flight-formation.jpg" />
+
+        </div>
+    </section>
+    </div>
 
 
 
