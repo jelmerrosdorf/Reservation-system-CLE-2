55fdd797f160d08257cde7494adb7555269dccdc
ThreeMAC <bram.vd.hidde@gmail.com>
2021-01-09 19:16:24 +0100

    Filled aboutpage

--- aboutpage.php
+++ aboutpage.php
@@ -1,1 +1,20 @@
 <?php
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>CutKapsel</title>
+    <link rel="stylesheet" href="./style.css"/>
+</head>
+<body>
+<header>Personalia</header>
+
+
+<div class="topnav">
+    <a class="inactive" href="./Homepage.php">Home</a>
+    <a class="inactive" href="./portfoliopage.php">Portfolio</a>
+    <a class="active" href="./aboutpage.php">Personalia</a>
+    <a class="inactive">Contact</a>
+    <a class="inactive" href="./appointmentpage.php">Reserveren</a>
+</div>
