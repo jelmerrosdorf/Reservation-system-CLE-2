a01360dfdd3b216b19986fb80f5b644eb549f718
ThreeMAC <bram.vd.hidde@gmail.com>
2021-01-10 22:10:24 +0100

    Edited portfoliopage files

--- portfoliopage.php
+++ portfoliopage.php
@@ -34,31 +34,31 @@
 <div class="grid">
     <div class="grid-sizer"></div>
     <div class="grid-item">
-        <img class="grid-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg"  alt=""/>
     </div>
     <div class="grid-item">
-        <img class="grid-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg" />
     </div>
     <div class="grid-item">
-        <img class="grid-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg" />
     </div>
     <div class="grid-item">
-        <img class="grid-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/one-world-trade.jpg" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/one-world-trade.jpg" />
     </div>
     <div class="grid-item">
-        <img class="grid-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/drizzle.jpg" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/drizzle.jpg" />
     </div>
     <div class="grid-item">
-        <img class="grid-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+        <img  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
     </div>
     <div class="grid-item">
-        <img class="grid-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/contrail.jpg" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/contrail.jpg" />
     </div>
     <div class="grid-item">
-        <img class="grid-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/golden-hour.jpg" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/golden-hour.jpg" />
     </div>
     <div class="grid-item">
-        <img class="grid-img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/flight-formation.jpg" />
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/flight-formation.jpg" />
     </div>
 </div>
 
