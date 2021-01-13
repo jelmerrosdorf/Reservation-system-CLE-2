cfb8a04cbabe44ae1443dea214847f86aae693a8
ThreeMAC <bram.vd.hidde@gmail.com>
2021-01-13 12:18:28 +0100

    Added correct pictures to portfoliopage.php

--- portfoliopage.php
+++ portfoliopage.php
@@ -34,31 +34,58 @@
 <div class="grid">
     <div class="grid-sizer"></div>
     <div class="grid-item">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg"  alt=""/>
+        <img src="https://i.imgur.com/hXkvkkh.jpg"  alt=""/>
     </div>
     <div class="grid-item">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg" />
+        <img src="https://i.imgur.com/rLXE0Tj.jpg" alt="" />
     </div>
     <div class="grid-item">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg" />
+        <img src="https://i.imgur.com/yfguRNS.jpg" alt="" />
     </div>
     <div class="grid-item">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/one-world-trade.jpg" />
+        <img src="https://i.imgur.com/PStXSmp.jpg" alt="" />
     </div>
     <div class="grid-item">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/drizzle.jpg" />
+        <img src="https://i.imgur.com/p6v7B25.jpg" alt="" />
     </div>
     <div class="grid-item">
-        <img  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/cat-nose.jpg" />
+        <img  src="https://i.imgur.com/2xFYjaL.jpg" alt="" />
     </div>
     <div class="grid-item">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/contrail.jpg" />
+        <img src="https://i.imgur.com/gNtRwCw.jpg" alt="" />
     </div>
     <div class="grid-item">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/golden-hour.jpg" />
+        <img src="https://i.imgur.com/IvKFQvG.jpg" alt="" />
     </div>
     <div class="grid-item">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/flight-formation.jpg" />
+        <img src="https://i.imgur.com/YasvU3H.jpg" alt="" />
+    </div>
+    <div class="grid-item">
+        <img src="https://i.imgur.com/WqFiygs.jpg" alt="" />
+    </div>
+    <div class="grid-item">
+        <img src="https://i.imgur.com/1nsYL4u.jpg" alt="" />
+    </div>
+    <div class="grid-item">
+        <img src="https://i.imgur.com/9Dbg8tP.jpg" alt="" />
+    </div>
+    <div class="grid-item">
+        <img src="https://i.imgur.com/6gATMTg.jpg" alt="" />
+    </div>
+    <div class="grid-item">
+        <img src="https://i.imgur.com/VQKol5N.jpg" alt="" />
+    </div>
+    <div class="grid-item">
+        <img src="https://i.imgur.com/lo3Z4oc.jpg" alt="" />
+    </div>
+    <div class="grid-item">
+        <img src="https://i.imgur.com/5vJeDiL.jpg" alt="" />
+    </div>
+    <div class="grid-item">
+        <img src="https://i.imgur.com/ZXhw9Dc.jpg" alt="" />
+    </div>
+    <div class="grid-item">
+        <img src="https://i.imgur.com/M66lhWZ.jpg" alt="" />
     </div>
 </div>
 
