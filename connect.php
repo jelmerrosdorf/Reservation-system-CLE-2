ae9ff8596f709e876573d5c70161daa2d79583c5
jelmerrosdorf <jelmerr99@gmail.com>
2021-01-15 21:20:26 +0100

    Added date and time to connect.php, they now get saved in db

--- connect.php
+++ connect.php
@@ -4,9 +4,13 @@ require_once "database.php";
 
 $name = filter_input(INPUT_POST, 'name');
 $email = filter_input(INPUT_POST, 'email');
+$date = filter_input(INPUT_POST, 'date');
+$time = filter_input(INPUT_POST, 'time');
 
 if (!empty($name)) {
 if (!empty($email)) {
+if (!empty($date)) {
+if (!empty($time)) {
 
 $host = "localhost";
 $dbusername = "root";
@@ -17,14 +21,13 @@ $dbname = "rssystem";
 
 $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
 
-
 if (mysqli_connect_error()) {
     die ('Connect error (' . mysqli_connect_errno() . ') '
         . mysqli_connect_error() );
 }
 else {
-    $sql = "INSERT INTO appointments (name, email)
-    values ('$name', '$email')";
+    $sql = "INSERT INTO appointments (name, email, date, time)
+    values ('$name', '$email', '$date', '$time')";
 
     if ($conn->query ($sql)) {
         echo "Je afspraak is goed doorgekomen!";
@@ -45,6 +48,17 @@ else{
     echo "Email mag niet leeg zijn!";
     die();
 }
+}
+else{
+    echo "Datum mag niet leeg zijn!";
+    die();
+}
+}
+else{
+    echo "Tijd mag niet leeg zijn!";
+    die();
+}
+
 
 
 
