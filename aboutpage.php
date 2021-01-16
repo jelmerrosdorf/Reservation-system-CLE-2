b1081fd9c5632773b9198505df851b370f2d1c0f
ThreeMAC <bram.vd.hidde@gmail.com>
2021-01-16 19:13:40 +0100

    Two divs on aboutpage.

--- aboutpage.php
+++ aboutpage.php
@@ -14,6 +14,28 @@
 <div class="topnav">
     <a class="inactive" href="./Homepage.php">Home</a>
     <a class="inactive" href="./portfoliopage.php">Portfolio</a>
-    <a class="active" href="./aboutpage.php">Personalia en Contact</a>
+    <a class="active" href="./aboutpage.php">Personalia en contact</a>
     <a class="inactive" href="./appointmentpage.php">Reserveren</a>
 </div>
+
+<div id="wrapper-home">
+    <div id="firsthome">
+        <div class="headerhome">
+            <h1><br>
+                Personalia
+            </h1><br>
+        </div>
+        <img src="https://i.imgur.com/7UAFh0p.jpg" alt="">
+    </div>
+    <div id="secondhome">
+        <div class="headerhome">
+            <h1><br>
+                Contact
+            </h1><br>
+
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
